a76691ee19f5234ca60d7967c581985d59f9c976
Bahlul19 <bahlul.tausif@gmail.com>
2021-05-17 12:21:45 +0600

    Added the page routing for the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const customRouter = (
+//   <Router>
+//     <div>
+//       <Route exact="/" component={HomePage}></Route>
+//       <Route path="/shop/hats"></Route>
+//       <Route path="/shop/jackets"></Route>
+//       <Route path="/shop/sneakers"></Route>
+//       <Route path="/shop/womens"></Route>
+//       <Route path="/shop/mens"></Route>
+//     </div>
+//   </Router>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reactDom.render(customRouter,document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>,
+  document.getElementById('root')
+  );
+
 reportWebVitals();
