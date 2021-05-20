a5eb14e01ad25cf2e87907e936a8da8274aba48f
Bahlul19 <bahlul.tausif@gmail.com>
2021-05-20 15:13:09 +0600

    created the signin form with style and created separate component for each functionality and design

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import Header from './components/header/header.component';
 
 // const HatsPage = () => (
@@ -21,6 +23,7 @@ function App() {
 
        <Route exact path="/" component={HomePage} />
        <Route path="/shop" component={ShopPage} />
+       <Route path="/signin" component={SignInAndSignUpPage}/>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,6 +20,7 @@ const Header = () => (
 
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT</Link>
+            <Link className='option' to='/signin'>SIGNIN</Link>
 
         </div>
     </div>
